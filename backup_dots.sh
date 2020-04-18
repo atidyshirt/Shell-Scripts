d6e94834ffefe8a5ec1d9da62c184ad04fd0ff89
Jordan Pyott <jordanpyott@gmail.com>
2020-04-18 18:51:56 +1200

    addded script to backup my dotfiles

---
+++ backup_dots.sh
@@ -0,0 +1,21 @@
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/bin
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/scripts
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/.config
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/.profile
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/.skhdrc
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/.Xresources
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/.zshrc
+rm -rf ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data
+
+cp-improved $HOME/bin/ ~/Documents/Files/Bash/MacOS-Dotfiles/bin 
+cp-improved $HOME/scripts ~/Documents/Files/Bash/MacOS-Dotfiles/
+cp-improved $HOME/.config/yabai/ ~/Documents/Files/Bash/MacOS-Dotfiles/.config/yabai
+cp-improved $HOME/.config/ranger/ ~/Documents/Files/Bash/MacOS-Dotfiles/.config/ranger
+cp-improved $HOME/.profile ~/Documents/Files/Bash/MacOS-Dotfiles/
+cp-improved $HOME/.skhdrc ~/Documents/Files/Bash/MacOS-Dotfiles/
+cp-improved $HOME/.Xresources ~/Documents/Files/Bash/MacOS-Dotfiles/
+cp-improved $HOME/.zshrc ~/Documents/Files/Bash/MacOS-Dotfiles/
+cp-improved $HOME/spicetify_data/Themes ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data/Themes
+cp-improved $HOME/spicetify_data/config.ini ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data/
+
+
