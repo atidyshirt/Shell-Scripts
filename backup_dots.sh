1ab4ba03898e59529aabe8bbb34f133acb6521d7
Jordan Pyott <jordanpyott@gmail.com>
2020-07-14 15:24:25 +1200

    Updating script to not double folders

--- backup_dots.sh
+++ backup_dots.sh
@@ -14,14 +14,14 @@ cp-improved $HOME/bin/ ~/Documents/Files/Bash/MacOS-Dotfiles/bin
 cp-improved $HOME/scripts ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.yabairc ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.config/ranger/ ~/Documents/Files/Bash/MacOS-Dotfiles/.config/ranger
-cp-improved $HOME/.config/alacritty ~/Documents/Files/Bash/MacOS-Dotfiles/.config/alacritty
+cp-improved $HOME/.config/alacritty ~/Documents/Files/Bash/MacOS-Dotfiles/.config/
 cp-improved $HOME/.profile ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.skhdrc ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.Xresources ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.zshrc ~/Documents/Files/Bash/MacOS-Dotfiles/
-cp-improved $HOME/spicetify_data/Themes ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data/Themes
+cp-improved $HOME/spicetify_data/Themes ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data/
 cp-improved $HOME/spicetify_data/config.ini ~/Documents/Files/Bash/MacOS-Dotfiles/spicetify_data/
-cp-improved $HOME/.wallpaper ~/Documents/Files/Bash/MacOS-Dotfiles/.wallpaper
+cp-improved $HOME/.wallpaper ~/Documents/Files/Bash/MacOS-Dotfiles/
 cp-improved $HOME/.tmux.conf ~/Documents/Files/Bash/MacOS-Dotfiles/
 
 
